1ee56740a3aedbc59bef7e02ad3a3deda71ff601
irfansofyana <irfansofyana0305@gmail.com>
2022-10-04 12:54:42 +0000

    [LeetCode Sync] Runtime - 3 ms (7.66%), Memory - 2.8 MB (96.10%)

--- submissions/1603-running-sum-of-1d-array/solution.go
+++ submissions/1603-running-sum-of-1d-array/solution.go
@@ -1,7 +1,13 @@
 func runningSum(nums []int) []int {
-    n := len(nums)
-    for i := 1; i < n; i++ {
-        nums[i] += nums[i-1]
+    sums := make([]int, len(nums))
+    
+    for i, num := range nums {
+        if i == 0 {
+            sums[i] = num
+            continue
+        }
+        sums[i] = sums[i-1] + num
     }
-    return nums
+    
+    return sums
 }
