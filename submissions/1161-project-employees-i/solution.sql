2194c3c3019bb23f23747d63f04154bd356a1c93
irfansofyana <irfansofyana0305@gmail.com>
2023-07-20 06:31:24 +0000

    [LeetCode Sync] Runtime - 2503 ms (5.04%), Memory - 0B (100.00%)

---
+++ submissions/1161-project-employees-i/solution.sql
@@ -0,0 +1,11 @@
+SELECT
+    project_id,
+    ROUND(AVG(experience_years),2) AS average_years
+FROM
+    Project AS p
+LEFT JOIN
+    Employee AS e
+ON 
+    p.employee_id = e.employee_id
+GROUP BY
+    project_id
