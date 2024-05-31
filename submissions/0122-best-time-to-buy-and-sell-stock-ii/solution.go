08d155433fc96ce1c6346039994ad8937e78f121
irfansofyana <irfansofyana0305@gmail.com>
2024-05-31 06:34:08 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 3.4 MB (13.71%)

---
+++ submissions/0122-best-time-to-buy-and-sell-stock-ii/solution.go
@@ -0,0 +1,9 @@
+func maxProfit(prices []int) int {
+    ans := 0
+    for i := 1; i < len(prices); i++ {
+        if prices[i] > prices[i-1] {
+            ans += prices[i] - prices[i-1]
+        }
+    }
+    return ans
+}
