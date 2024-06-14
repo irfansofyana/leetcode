9c8e86f850725a311497778772a8cd7944796c40
irfansofyana <irfansofyana0305@gmail.com>
2024-06-14 16:36:01 +0000

    [LeetCode Sync] Runtime - 149 ms (61.29%), Memory - 109.6 MB (57.16%)

---
+++ submissions/2022-maximum-alternating-subsequence-sum/solution.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    long long dp[100003][3];
+    vector<int> nums;
+
+    long long maxAlternatingSum(vector<int>& nums) {
+        memset(dp, -1, sizeof dp);
+        this->nums = nums;
+        return findMax(0, 0);
+    }
+
+    long long findMax(int pos, int parity) {
+        if (pos == nums.size()) {
+            return 0;
+        }
+
+        if (dp[pos][parity] != -1) {
+            return dp[pos][parity];
+        }
+
+        long long maks = findMax(pos+1, parity);
+        long long val;
+        if (parity % 2 == 0) {
+            val = findMax(pos+1, (parity+1)%2) + 1LL*nums[pos];
+        } else {
+            val = findMax(pos+1, (parity+1)%2) - 1LL*nums[pos];
+        }
+        maks = max(maks, val);
+
+        return dp[pos][parity] = maks;
+    }
+};
